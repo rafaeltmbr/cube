00b7ad693e6e0b6185e3cbc51f71af920d9adb02
Rafhael Payão <51467263+rafaeltmbr@users.noreply.github.com>
2020-11-22 11:14:09 -0300

    fix: limit height incresing to avoid bad movementation

--- scripts.js
+++ scripts.js
@@ -8,11 +8,18 @@ function implementCubeMovement() {
   function allowCubeMovement(startEvent) {
     const isTouch = startEvent.touches;
     const { pageX, pageY } = isTouch ? startEvent.touches[0] : startEvent;
+    const last = { x: pageX, y: pageY };
+    offset.y = lastOffset.y;
+    offset.x = lastOffset.x;
 
     function handleCubeMovement(moveEvent) {
       const { pageX: pX, pageY: pY } = isTouch ? moveEvent.touches[0] : moveEvent;
-      offset.x = (pX - pageX + lastOffset.x) % 360;
-      offset.y = (pY - pageY + lastOffset.y) % 360;
+      offset.x += pX - last.x;
+      offset.y += pY - last.y;
+      offset.x = offset.x % 360;
+      offset.y = offset.y >= 45 ? 45 : offset.y <= -45 ? -45 : offset.y;
+      last.y = pY;
+      last.x = pX;
       cube.style = `--rotateX: ${-offset.y}deg; --rotateY: ${offset.x}deg;`;
     }
 
